0e706e588896395794537fb1142b8875d5899060
iBryan6 <bryan.argandona@Bryan-Argandona.local>
2020-03-30 18:13:34 -0400

    Basic-Func/create a new page

--- cypress/integration/cms/staging/basic_funcionality.spec.js
+++ cypress/integration/cms/staging/basic_funcionality.spec.js
@@ -11,18 +11,37 @@ context("Basic Functionalities", () => {
 
   it("Login to CMS", () => {
     cy.visit("https://auth.g5search.com");
+    //Type Email
     cy.get("#user_email")
       .type(Cypress.env("cms_auth_email"))
       .should("have.value", Cypress.env("cms_auth_email"));
+    //Type wrong password
     cy.get("#user_password").type("wrong_password");
     cy.contains("Sign In").click();
     cy.contains("Invalid email or password.").should("be.visible");
+    //Type correct password
     cy.get("#user_password").type(Cypress.env("cms_auth_password"));
     cy.contains("Sign In").click();
     cy.contains("Signed in successfully.").should("be.visible");
   });
 
   it("Create a new Page in a location", () => {
-    cy.contains("BRYAN TESTBED");
+    //Load first location edit
+    cy.xpath(
+      "/html/body/div[5]/main/div/div/div/div/div/div[2]/div[5]/ul[2]/li[1]/div/div/div[2]/div[3]/a[1]"
+    ).click();
+    cy.contains(" Create a New Page").should("be.visible");
+    //Create new page
+    cy.contains(" Create a New Page").click({ force: true });
+    cy.xpath(
+      "/html/body/div[5]/main/div/div/div/div[4]/div/div/div[4]/div/ul/li[1]/div/div[2]/div[1]/div[1]/input"
+    )
+      .type("NEW PAGE")
+      .should("have.value", "NEW PAGE");
+    cy.xpath(
+      "/html/body/div[5]/main/div/div/div/div[4]/div/div/div[4]/div/ul/li[1]/div/div[2]/div[1]/div[5]/div[2]/div/button[1]"
+    ).click();
+    cy.contains("Success").should("be.visible");
+    cy.contains("NEW PAGE").should("be.visible");
   });
 });
