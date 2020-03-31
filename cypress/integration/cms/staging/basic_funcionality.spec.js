4f102597ff3fba619256400c06302c1259ebc577
iBryan6 <bryan.argandona@Bryan-Argandona.local>
2020-03-30 23:27:53 -0400

    Delete page if it already exists.

--- cypress/integration/cms/staging/basic_funcionality.spec.js
+++ cypress/integration/cms/staging/basic_funcionality.spec.js
@@ -1,6 +1,9 @@
 /// <reference types="cypress" />
 
 context("Basic Functionalities", () => {
+  //VARIABLES
+  let pageName = "NEW PAGE";
+
   beforeEach(() => {
     cy.visit(Cypress.env("BASE_URL_STAGING"));
     Cypress.Cookies.preserveOnce(
@@ -25,23 +28,47 @@ context("Basic Functionalities", () => {
     cy.contains("Signed in successfully.").should("be.visible");
   });
 
-  it("Create a new Page in a location", () => {
+  it.only("Create new Page in a location", () => {
     //Load first location edit
     cy.xpath(
       "/html/body/div[5]/main/div/div/div/div/div/div[2]/div[5]/ul[2]/li[1]/div/div/div[2]/div[3]/a[1]"
     ).click();
-    cy.contains(" Create a New Page").should("be.visible");
+    cy.wait(5000);
+    cy.contains("h4", "Navigation Pages").should("be.visible");
+ 
+
+    //Check if pages exists
+    cy.xpath("/html/body/div[5]/main/div/div/div/div[4]/div/div/div[4]").within(
+      body => {
+        if (body.find("span").length > 0) {
+          cy.get("span.name-text").then($body => {
+            if ($body.text().includes(pageName)) {
+              //Deletes page if it exists
+              cy.xpath(
+                `//span[.='${pageName}']/following-sibling::span/a[.=' Settings ']`
+              ).click();
+              cy.get(".btn.delete-action.delete.ember-view").click();
+              cy.wait(5000);
+              cy.xpath("/html/body/div[16]/div[7]/div/button").should("be.visible");
+              cy.get("/html/body/div[16]/div[7]/div/button").click();
+            }
+          });
+        }
+      }
+    );
+
     //Create new page
+    cy.wait(5000);
     cy.contains(" Create a New Page").click({ force: true });
     cy.xpath(
       "/html/body/div[5]/main/div/div/div/div[4]/div/div/div[4]/div/ul/li[1]/div/div[2]/div[1]/div[1]/input"
     )
-      .type("NEW PAGE")
-      .should("have.value", "NEW PAGE");
+      .type(pageName)
+      .should("have.value", pageName);
     cy.xpath(
       "/html/body/div[5]/main/div/div/div/div[4]/div/div/div[4]/div/ul/li[1]/div/div[2]/div[1]/div[5]/div[2]/div/button[1]"
     ).click();
     cy.contains("Success").should("be.visible");
-    cy.contains("NEW PAGE").should("be.visible");
+    cy.contains(pageName).should("be.visible");
   });
 });
